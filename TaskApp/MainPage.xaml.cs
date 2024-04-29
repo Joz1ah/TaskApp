8cf16d1ccdbc37b001dcaabe824bfc9f7f8488cf
Hermosura, Joshua <joshua.hermosura@accenture.com>
2024-04-29 19:17:38 +0800

    Add project files.

---
+++ TaskApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace TaskApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private bool _secondButtonEnabled = true;
+        public bool SecondButtonEnabled
+        {
+            get => _secondButtonEnabled;
+            set
+            {
+                if (_secondButtonEnabled != value)
+                {
+                    _secondButtonEnabled = value;
+                    OnPropertyChanged(nameof(SecondButtonEnabled));
+                    ((Command)IncrementCounterCommand).ChangeCanExecute();
+                }
+            }
+        }
+
+        private int _counter = 0;
+        public int Counter
+        {
+            get => _counter;
+            set
+            {
+                if (_counter != value)
+                {
+                    _counter = value;
+                    OnPropertyChanged(nameof(Counter));
+                }
+            }
+        }
+
+        public ICommand ToggleSecondButtonEnabledCommand { get; }
+        public ICommand IncrementCounterCommand { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ToggleSecondButtonEnabledCommand = new Command(() => SecondButtonEnabled = !SecondButtonEnabled);
+            IncrementCounterCommand = new Command(() => Counter++);
+            BindingContext = this;
+        }
+    }
+}
